f122c87a65a3482962dfc11d88e64f5037e7c0bc
EstonianGuy <74711970+EstonianGuy@users.noreply.github.com>
2021-03-20 10:48:57 +0300

    Add files via upload

---
+++ components/welcome.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<meta charset = "UTF-8">
+<link rel="stylesheet" href="style_welcome.css">
+<title> Средневековая архитектура </title> 
+</head>
+<p><a class="body"><center><strong><?php echo('Добро пожаловать на сайт, посвященный архитектуре!') ?></strong></center></a></p>
+<center><a href="main.php" class="buttonk">Перейти на сайт</a></center>
